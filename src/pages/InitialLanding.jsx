10aac356428cd934d2af959039cd7bb9d6d5410d
muskaanshah <hmuskaan11@gmail.com>
2022-03-23 14:09:46 +0530

    feat: focus displayed

--- src/pages/InitialLanding.jsx
+++ src/pages/InitialLanding.jsx
@@ -1,15 +1,29 @@
 import "./landingpage.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function InitialLanding() {
+	const [name, setName] = useState("");
+	const inputHandler = (e) => {
+		setName(e.target.value);
+		localStorage.setItem("name", e.target.value);
+	};
 	return (
 		<div className="landingimage overlay-wrapper">
 			<div className="overlay">
 				<p className="fw-600 nametag mb-1">Hello, what's your name?</p>
-				<input type="text" className="fw-500 input-text name-text" />
-				<Link to="/landing" className="btn bg-white borderradius-2">
-					<span>Continue ›</span>
-				</Link>
+				<input
+					type="text"
+					className="fw-500 input-text name-text"
+					value={name}
+					onChange={(e) => inputHandler(e)}
+					autocomplete="off"
+				/>
+				{name.length > 0 && (
+					<Link to="/landing" className="btn bg-white borderradius-2">
+						<span>Continue ›</span>
+					</Link>
+				)}
 			</div>
 		</div>
 	);
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,19 +1,48 @@
 import { Todo } from "../components/Todo/Todo";
 import "./landingpage.css";
+import { useEffect, useState } from "react";
 
 function Landing() {
+	const [userName, setUserName] = useState("");
+	const [focus, setFocus] = useState("");
+
+	useEffect(() => {
+		const user = localStorage.getItem("name");
+		setUserName(() => user);
+		const focus = localStorage.getItem("Focus");
+		setFocus(() => focus);
+	}, []);
+
+	const inputFocusHandler = (e) => {
+		localStorage.setItem("Focus", e.target.value);
+		const focus = localStorage.getItem("Focus");
+		setFocus(() => focus);
+	};
 	return (
 		<div className="landingimage overlay-wrapper">
 			<div className="overlay">
 				<p className="time-display fw-500 mb-0">11:08</p>
-				<p className="fw-600 nametag mt-0 mb-0-5">Good evening, Musk</p>
+				<p className="fw-600 nametag mt-0 mb-0-5">Good evening, {userName}</p>
 				<p className="fw-500 focustag my-0">
 					What's your main focus for today?
 				</p>
-				{/* Please note - Only one of the below two will be displayed, added both just for designing UI */}
-				<p className="fw-500 mt-2">TODAY</p>
-				<p className="my-0 fs-1-5 fw-600">Finish UI</p>
-				<input type="text" className="fw-500 name-text focus-text" />
+
+				{focus ? (
+					<>
+						<p className="fw-500 mt-2">TODAY</p>
+						<p className="my-0 fs-1-5 fw-600">{focus}</p>
+					</>
+				) : (
+					<input
+						type="text"
+						className="fw-500 name-text focus-text"
+						onKeyPress={(e) => {
+							if (e.key === "Enter") {
+								inputFocusHandler(e);
+							}
+						}}
+					/>
+				)}
 				<p className="todotag-bottom-right">Todo</p>
 				<div className="weather-top-right">
 					<p className="fs-1-25 fw-500 mb-0">27°</p>
