40dfbc9608b70e224c42cc638d85b3e36e673498
muskaanshah <hmuskaan11@gmail.com>
2022-03-23 21:58:25 +0530

    feat: converting time format

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -15,11 +15,15 @@ const initialState = {
 function Landing() {
 	const [state, dispatch] = useReducer(reducerFunc, initialState);
 	const [today, setDate] = useState(new Date());
+	const [is24HourFormat, setTo24HourFormat] = useState(true);
 	const hour = today.getHours();
 	const wish = `Good ${
-		(hour < 12 && "morning") || (hour < 17 && "afternoon") || "evening"
+		(hour < 12 && "morning") || (hour < 16 && "afternoon") || "evening"
 	}`;
-	const time12 = `${today.getHours()}:${today.getMinutes()}`;
+	const time24 = `${today.getHours()}:${today.getMinutes()}`;
+	const AmOrPm = hour >= 12 ? "PM" : "AM";
+	const hours = hour % 12 || 12;
+	const time12 = `${hours}:${today.getMinutes()} ${AmOrPm}`;
 	useEffect(() => {
 		const user = localStorage.getItem("name");
 		dispatch({ type: "SET_USERNAME", payload: { value: user } });
@@ -32,7 +36,17 @@ function Landing() {
 	return (
 		<div className="landingimage overlay-wrapper">
 			<div className="overlay">
-				<p className="time-display fw-500 mb-0">{time12}</p>;
+				<div className="centered">
+					<p className="time-display fw-500 mb-0">
+						{is24HourFormat ? time24 : time12}
+					</p>
+					<button
+						className="btn-focusaction"
+						onClick={() => setTo24HourFormat((prev) => !prev)}
+					>
+						<span class="material-icons-outlined">repeat</span>
+					</button>
+				</div>
 				<p className="fw-600 nametag mt-0 mb-0-5">
 					{wish}, {state.userName}
 				</p>
