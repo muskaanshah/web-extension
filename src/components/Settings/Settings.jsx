06d4d512077f13d9ba3e1c6beada25bb57a0011b
Muskaan Shah <hmuskaan11@gmail.com>
2023-07-30 01:56:03 +0530

    feat: mantras

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,72 +1,94 @@
-import { useState, useRef, useEffect } from "react";
-import { useOnClickOutside } from "../../hooks/useOnClickOutside";
-import { changeQuoteHandler } from "../Quote/Quote";
+import { useState, useRef, useEffect } from 'react';
+import { useOnClickOutside } from '../../hooks/useOnClickOutside';
+import { changeQuoteHandler } from '../Quote/Quote';
+import { calculateMantra } from '../../pages/Landing';
 
-export function Settings({ setQuote, setUpdateQuote }) {
-    const [settingsModal, setSettingsModal] = useState(false);
-    const [isRetainTodos, setIsRetainTodos] = useState(false);
-    const changeNameHandler = () => {
-        localStorage.setItem("name", "");
-        window.location.reload(false);
-    };
-    const modalRef = useRef();
-    const toggleRef = useRef();
-    useOnClickOutside(modalRef, toggleRef, () => setSettingsModal(false));
-    useEffect(() => {
-        if (localStorage.getItem("retain_todos") === null) {
-            localStorage.setItem("retain_todos", false);
-        }
-        setIsRetainTodos(JSON.parse(localStorage.getItem("retain_todos")));
-    }, []);
-    const changeRetainTodosHander = (e) => {
-        localStorage.setItem("retain_todos", !isRetainTodos);
-        setIsRetainTodos((isRetainTodos) => !isRetainTodos);
-    };
-    return (
-        <div className="settings-hover">
-            {settingsModal && (
-                <div
-                    className="ml-1 modal-settings borderradius-0-5 p-1"
-                    ref={modalRef}
-                >
-                    <span
-                        className="btn btn-change-name ml-0 px-0 pt-0"
-                        onClick={() => {
-                            changeNameHandler();
-                        }}
-                    >
-                        Change name
-                    </span>
-                    <div className="divider-black bg-grey-10 width-100" />
-                    <span
-                        className="btn btn-change-name ml-0 px-0"
-                        onClick={() => {
-                            changeQuoteHandler(setQuote, setUpdateQuote);
-                        }}
-                    >
-                        Change quote
-                    </span>
-                    <div className="divider-black bg-grey-10 width-100" />
-                    <div className="retain-todos-label pt-1">
-                        <p className="fs-0-8 my-0">Retain todos after 24 hours</p>
-                        <input
-                            type="checkbox"
-                            className="todo-checkbox"
-                            checked={isRetainTodos}
-                            onClick={() => {
-                                changeRetainTodosHander();
-                            }}
-                        />
-                    </div>
-                </div>
-            )}
-            <span
-                ref={toggleRef}
-                className="material-icons-outlined"
-                onClick={() => setSettingsModal((prev) => !prev)}
-            >
-                settings
-            </span>
-        </div>
-    );
+export function Settings({
+	setQuote,
+	setUpdateQuote,
+	showMantras,
+	setShowMantras,
+}) {
+	const [settingsModal, setSettingsModal] = useState(false);
+	const [isRetainTodos, setIsRetainTodos] = useState(false);
+	const changeNameHandler = () => {
+		localStorage.setItem('name', '');
+		window.location.reload(false);
+	};
+	const modalRef = useRef();
+	const toggleRef = useRef();
+	useOnClickOutside(modalRef, toggleRef, () => setSettingsModal(false));
+	useEffect(() => {
+		if (localStorage.getItem('retain_todos') == null) {
+			localStorage.setItem('retain_todos', false);
+		}
+		setIsRetainTodos(JSON.parse(localStorage.getItem('retain_todos')));
+	}, []);
+	const changeRetainTodosHander = (e) => {
+		localStorage.setItem('retain_todos', !isRetainTodos);
+		setIsRetainTodos((isRetainTodos) => !isRetainTodos);
+	};
+	const changeShowMantrasHander = () => {
+		const mantraToShow = calculateMantra();
+		setShowMantras({
+			...showMantras,
+			enabled: !showMantras.enabled,
+			mantra: !showMantras.enabled ? mantraToShow : '',
+		});
+	};
+	return (
+		<div className='settings-hover'>
+			{settingsModal && (
+				<div
+					className='ml-1 modal-settings borderradius-0-5 p-1'
+					ref={modalRef}
+				>
+					<span
+						className='btn btn-change-name ml-0 px-0 pt-0'
+						onClick={changeNameHandler}
+					>
+						Change name
+					</span>
+					<div className='divider-black bg-grey-10 width-100' />
+					<span
+						className='btn btn-change-name ml-0 px-0'
+						onClick={() => {
+							changeQuoteHandler(setQuote, setUpdateQuote);
+						}}
+					>
+						Change quote
+					</span>
+					<div className='divider-black bg-grey-10 width-100' />
+					<div className='retain-todos-label py-1'>
+						<p className='fs-0-8 my-0'>
+							Retain todos after 24 hours
+						</p>
+						<input
+							type='checkbox'
+							className='todo-checkbox'
+							checked={isRetainTodos}
+							onClick={changeRetainTodosHander}
+						/>
+					</div>
+					<div className='divider-black bg-grey-10 width-100' />
+					<div className='retain-todos-label pt-1'>
+						<p className='fs-0-8 my-0'>Show Mantras</p>
+						<input
+							type='checkbox'
+							className='todo-checkbox'
+							checked={showMantras.enabled}
+							onClick={changeShowMantrasHander}
+						/>
+					</div>
+				</div>
+			)}
+			<span
+				ref={toggleRef}
+				className='material-icons-outlined'
+				onClick={() => setSettingsModal((prev) => !prev)}
+			>
+				settings
+			</span>
+		</div>
+	);
 }
--- src/components/TimeDisplay/TimeDisplay.jsx
+++ src/components/TimeDisplay/TimeDisplay.jsx
@@ -1,82 +1,114 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { calculateMantra } from '../../pages/Landing';
 
-function TimeDisplay() {
-    const [userName, setUserName] = useState("");
-    useEffect(() => {
-        const user = localStorage.getItem("name");
-        setUserName(user);
-    }, []);
-    const [today, setDate] = useState(new Date());
-    const [is24HourFormat, setTo24HourFormat] = useState(true);
-    const hour = today.getHours();
-    const minute = today.getMinutes();
-    const minutes = minute / 10 < 1 ? `0${minute}` : minute;
-    const wish = `Good ${
-        (hour < 4 && "night") ||
-        (hour < 12 && "morning") ||
-        (hour < 16 && "afternoon") ||
-        (hour < 21 && "evening") ||
-        "night"
-    }`;
-    const time24 = `${hour}:${minutes}`;
-    const day = today.toLocaleDateString({ weekday: "long" });
-    const hours = hour % 12 || 12;
-    const time12 = `${hours}:${minutes}`;
-    const setupTime = localStorage.getItem("setupTime");
-    const retainTodos = JSON.parse(localStorage.getItem("retain_todos"));
-    if (setupTime === null) {
-        localStorage.setItem("setupTime", day);
-    } else {
-        if (day !== setupTime) {
-            localStorage.removeItem("Focus");
-            localStorage.removeItem("Quote");
-            if (retainTodos === false) {
-                localStorage.removeItem("Todos");
-            }
-			else {
-				const todos = JSON.parse(localStorage.getItem("Todos"));
-				const filteredTodos = todos.filter(x => x.todoCompleted === false);
-				localStorage.setItem("Todos", JSON.stringify(filteredTodos));
+function TimeDisplay({ setShowMantras }) {
+	const [userName, setUserName] = useState('');
+	useEffect(() => {
+		const user = localStorage.getItem('name');
+		setUserName(user);
+	}, []);
+	const [today, setDate] = useState(new Date());
+	const [is24HourFormat, setTo24HourFormat] = useState(true);
+	const hour = today.getHours();
+	const minute = today.getMinutes();
+	const minutes = minute / 10 < 1 ? `0${minute}` : minute;
+	const wish = `Good ${
+		(hour < 4 && 'night') ||
+		(hour < 12 && 'morning') ||
+		(hour < 16 && 'afternoon') ||
+		(hour < 21 && 'evening') ||
+		'night'
+	}`;
+	const time24 = `${hour}:${minutes}`;
+	const day = today.toLocaleDateString({ weekday: 'long' });
+	const hours = hour % 12 || 12;
+	const time12 = `${hours}:${minutes}`;
+	const setupTime = localStorage.getItem('setupTime');
+	const retainTodos = JSON.parse(localStorage.getItem('retain_todos'));
+	const mantras = localStorage.getItem('mantras')
+		? JSON.parse(localStorage.getItem('mantras'))
+		: null;
+	if (setupTime === null) {
+		localStorage.setItem('setupTime', day);
+	} else {
+		if (day !== setupTime) {
+			localStorage.removeItem('Focus');
+			localStorage.removeItem('Quote');
+			if (mantras?.enabled) {
+				const mantraToShow = calculateMantra();
+				setShowMantras({ ...mantras, mantra: mantraToShow });
 			}
-            localStorage.setItem("setupTime", day);
-        }
-    }
+			if (retainTodos === false) {
+				localStorage.removeItem('Todos');
+			} else {
+				const todos = JSON.parse(localStorage.getItem('Todos'));
+				const filteredTodos = todos.filter(
+					(x) => x.todoCompleted === false
+				);
+				localStorage.setItem('Todos', JSON.stringify(filteredTodos));
+			}
+			localStorage.setItem('setupTime', day);
+		}
+	}
 
-    useEffect(() => {
-        const timeformat = localStorage?.getItem("Timeformat");
-        timeformat
-            ? setTo24HourFormat(JSON.parse(timeformat))
-            : localStorage.setItem("Timeformat", is24HourFormat);
-    }, [is24HourFormat]);
+	const skipMantra = (mantraId) => {
+		const mantras = JSON.parse(localStorage.getItem('mantras'));
+		setShowMantras({
+			...mantras,
+			skipped: [...mantras.skipped, mantraId],
+			mantra: calculateMantra(),
+		});
+	};
 
-    useEffect(() => {
-        setInterval(() => {
-            setDate(() => new Date());
-        }, 1000);
-    }, []);
-    return (
-        <>
-            <div className="time-hover centered">
-                <p className="time-display fw-500 mb-0">
-                    {is24HourFormat ? time24 : time12}
-                </p>
-                <button
-                    className="btn-focusaction btn-time-change centered"
-                    onClick={() => {
-                        setTo24HourFormat((prev) => {
-                            localStorage.setItem("Timeformat", !prev);
-                            return !prev;
-                        });
-                    }}
-                >
-                    <span className="material-icons-outlined">repeat</span>
-                </button>
-            </div>
-            <p className="fw-600 nametag mt-0 mb-1">
-                {wish}, {userName}
-            </p>
-        </>
-    );
+	useEffect(() => {
+		const timeformat = localStorage?.getItem('Timeformat');
+		timeformat
+			? setTo24HourFormat(JSON.parse(timeformat))
+			: localStorage.setItem('Timeformat', is24HourFormat);
+	}, [is24HourFormat]);
+
+	useEffect(() => {
+		setInterval(() => {
+			setDate(() => new Date());
+		}, 1000);
+	}, []);
+	return (
+		<>
+			<div className='time-hover centered'>
+				<p className='time-display fw-500 mb-0'>
+					{is24HourFormat ? time24 : time12}
+				</p>
+				<button
+					className='btn-focusaction btn-time-change centered'
+					onClick={() => {
+						setTo24HourFormat((prev) => {
+							localStorage.setItem('Timeformat', !prev);
+							return !prev;
+						});
+					}}
+				>
+					<span className='material-icons-outlined'>repeat</span>
+				</button>
+			</div>
+			{mantras?.enabled ? (
+				<div className='centered mantras-hover'>
+					<p className='fw-600 nametag mt-0 mb-1'>
+						{mantras?.mantra?.text}
+					</p>
+					<button
+						className='btn-focusaction centered btn-mantras-skip'
+						onClick={() => skipMantra(mantras?.mantra?.id)}
+					>
+						Skip
+					</button>
+				</div>
+			) : (
+				<p className='fw-600 nametag mt-0 mb-1'>
+					{wish}, {userName}
+				</p>
+			)}
+		</>
+	);
 }
 
 export { TimeDisplay };
