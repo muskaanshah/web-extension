028b92b3e54ac4aab6741add753792ea3cef56b8
Muskaan Shah <hmuskaan11@gmail.com>
2023-07-30 00:26:32 +0530

    feat: delete note

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,13 +1,17 @@
-import { useEffect, useState } from "react";
-import "./notes.css";
+import { useEffect, useState } from 'react';
+import './notes.css';
 
 export function Notes() {
 	const [notesModal, setNotesModal] = useState(false);
-	const [notes, setNotes] = useState({ title: "", desc: "" });
-	const notesObj = JSON.parse(localStorage.getItem("notes_content"));
+	const [notes, setNotes] = useState({ title: '', desc: '' });
+	const notesObj = JSON.parse(localStorage.getItem('notes_content'));
+	const deleteNote = () => {
+		localStorage.removeItem('notes_content');
+		setNotes({ title: '', desc: '' });
+	};
 	useEffect(() => {
 		if (
-			localStorage.getItem("notes_content") != null &&
+			localStorage.getItem('notes_content') != null &&
 			(notesObj?.title.length > 0 || notesObj?.desc.length > 0)
 		) {
 			setNotesModal(true);
@@ -15,32 +19,44 @@ export function Notes() {
 		}
 	}, [notesObj?.desc, notesObj?.title, setNotesModal]);
 	return (
-		<div className="notes-wrapper">
+		<div className='notes-wrapper'>
 			<div
-				className="centered mb-0-5 cursor-pointer"
+				className='centered mb-0-5 cursor-pointer'
 				onClick={() => {
 					setNotesModal((prev) => !prev);
 				}}
 			>
-				<p className="my-0">Notes</p>
-				<span className="material-icons-outlined">edit_note</span>
+				<p className='my-0'>Notes</p>
+				<span className='material-icons-outlined'>edit_note</span>
 			</div>
 			{notesModal && (
-				<div className="notes-container">
+				<div className='notes-container'>
 					<button
-						className="btn-todo btn-close-countdownmodal mr-0-5 centered"
+						className='btn-todo btn-close-countdownmodal mr-0-5 centered'
 						onClick={() => setNotesModal(false)}
 					>
-						<span className="material-icons-outlined">close</span>
+						<span className='material-icons-outlined'>
+							expand_less
+						</span>
 					</button>
+					{(notes.title.length > 0 || notes.desc.length > 0) && (
+						<button
+							className='btn-todo btn-close-countdownmodal mr-2 centered'
+							onClick={deleteNote}
+						>
+							<span className='material-icons-outlined'>
+								delete
+							</span>
+						</button>
+					)}
 					<input
-						type="text"
-						placeholder="Add a title"
-						className="notes-title-input pb-0-5 mr-1"
+						type='text'
+						placeholder='Add a title'
+						className='notes-title-input pb-0-5 mr-1'
 						value={notes.title}
 						onChange={(e) => {
 							localStorage.setItem(
-								"notes_content",
+								'notes_content',
 								JSON.stringify({
 									title: e.target.value,
 									desc: notes.desc,
@@ -53,13 +69,13 @@ export function Notes() {
 						}}
 					></input>
 					<textarea
-						className="notes-textarea"
-						placeholder="Start typing something"
+						className='notes-textarea'
+						placeholder='Start typing something'
 						value={notes.desc}
 						spellCheck={false}
 						onChange={(e) => {
 							localStorage.setItem(
-								"notes_content",
+								'notes_content',
 								JSON.stringify({
 									title: notes.title,
 									desc: e.target.value,
